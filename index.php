a2f40f107ef31298b1aa6f7b78a3cfe23501ba4c
Omega-19 <vigannicole1@gmail.com>
2023-09-05 14:17:18 +0100

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-CRUD
-    <strong>VARCHAR</strong> remplace le string dans le remplissage d'une table en phpmyadmin.
-
-    <strong>TEXT</strong> est préférable qd on doit utiliser une description par exemple dans le remplissage d'une table en phpmyadmin
-
-    
